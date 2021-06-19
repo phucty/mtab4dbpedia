4cd9e86121beeb25a35f39aaa1e4a52679a2ff5d
Phuc Nguyen <phuc.nii.ac.jp@gmail.com>
2021-06-19 17:22:34 +0900

    Test API

--- experiments/mtab4d.py
+++ experiments/mtab4d.py
@@ -239,24 +239,3 @@ def m_test_semtab(round_id=1, data_version="semtab_2019_dbpedia_2016-10", n_thre
     )
     m_test_evaluation(round_id, data_version=data_version)
     print(f"{str(timedelta(seconds=round(time() - start)))}")
-
-
-if __name__ == "__main__":
-    mtab_api = MTab4D()
-    # 1. Entity Search
-    print(mtab_api.search_entity("Tokyo"))
-
-    # 2. Get entity information
-    print(mtab_api.get_entity_info("Tokyo"))
-
-    # # 3. Table annotation
-    for round_id in [1, 2, 3, 4]:
-        m_test_semtab(round_id=round_id, n_thread=4, data_version="semtab_org")
-        m_test_semtab(
-            round_id=round_id, n_thread=4, data_version="semtab_2019_dbpedia_2016-10"
-        )
-
-    # 4. Evaluation
-    for round_id in [1, 2, 3, 4]:
-        m_test_evaluation(round_id, data_version="semtab_org")
-        m_test_evaluation(round_id, data_version="semtab_2019_dbpedia_2016-10")
---
+++ m_main.py
@@ -0,0 +1,21 @@
+from experiments.mtab4d import MTab4D, m_test_semtab, m_test_evaluation
+
+if __name__ == "__main__":
+    mtab_api = MTab4D()
+    # 1. Entity Search
+    print(mtab_api.search_entity("Tokyo"))
+
+    # 2. Get entity information
+    print(mtab_api.get_entity_info("Tokyo"))
+
+    # # 3. Table annotation
+    for round_id in [1, 2, 3, 4]:
+        m_test_semtab(round_id=round_id, n_thread=4, data_version="semtab_org")
+        m_test_semtab(
+            round_id=round_id, n_thread=4, data_version="semtab_2019_dbpedia_2016-10"
+        )
+
+    # 4. Evaluation
+    for round_id in [1, 2, 3, 4]:
+        m_test_evaluation(round_id, data_version="semtab_org")
+        m_test_evaluation(round_id, data_version="semtab_2019_dbpedia_2016-10")
